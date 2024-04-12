dd6aaa22a8c3dc195df9cba1f1565870b3a32fc7
rodriRoddl <rodrigo.lobosc@usm.cl>
2024-04-12 01:48:59 -0400

    restore index.js

---
+++ AppWeb/routes/index.js
@@ -0,0 +1,36 @@
+'use strict'
+
+const express = require('express');
+const path = require('path'); // Importar el módulo path
+const router = express.Router();
+
+router
+.get('/', (req, res) => {
+    // Usar path.join para construir la ruta de forma segura
+    res.sendFile(path.join(__dirname, '..', 'views', 'index.html'));
+})
+.get('/integrantes', (req, res) => {
+    // Usar path.join para asegurar compatibilidad entre sistemas operativos
+    res.sendFile(path.join(__dirname, '..', 'views', 'integrantes.html'));
+})
+.get('/acerca', (req, res) => {
+    // Igual que arriba, usando path.join
+    res.sendFile(path.join(__dirname, '..', 'views', 'acerca.html'));
+})
+
+.get('/login', (req, res) => {
+    // Igual que arriba, usando path.join
+    res.sendFile(path.join(__dirname, '..', 'views', 'login.html'));
+})
+
+.get('/register', (req, res) => {
+    // Igual que arriba, usando path.join
+    res.sendFile(path.join(__dirname, '..', 'views', 'register.html'));
+})
+
+.get('/reserva', (req, res) => {
+    // Igual que arriba, usando path.join
+    res.sendFile(path.join(__dirname, '..', 'views', 'reserva.html'));
+})
+
+module.exports = router;

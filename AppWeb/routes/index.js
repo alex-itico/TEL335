be45f1c283fd2a0b77740042f3430a0b902fd18f
YizzHub <jachaffe@gmail.com>
2024-04-17 16:56:12 -0400

    creacion faq

--- AppWeb/routes/index.js
+++ AppWeb/routes/index.js
@@ -5,12 +5,10 @@ const path = require('path'); // Importar el módulo path
 const router = express.Router();
 
 router
-
 .get('/', (req, res) => {
-    // Igual que arriba, usando path.join
-    res.sendFile(path.join(__dirname, '..', 'views', 'index_2.html'));
+    // Usar path.join para construir la ruta de forma segura
+    res.sendFile(path.join(__dirname, '..', 'views', 'index.html'));
 })
-
 .get('/integrantes', (req, res) => {
     // Usar path.join para asegurar compatibilidad entre sistemas operativos
     res.sendFile(path.join(__dirname, '..', 'views', 'integrantes.html'));
@@ -45,9 +43,9 @@ router
     res.sendFile(path.join(__dirname, '..', 'views', 'nueva.html'));
 })
 
-.get('/ver_reservas', (req, res) => {
+.get('/faq', (req, res) => {
     // Igual que arriba, usando path.join
-    res.sendFile(path.join(__dirname, '..', 'views', 'ver_reservas_adm.html'));
+    res.sendFile(path.join(__dirname, '..', 'views', 'faq.html'));
 })
 
 .get('/perfil', (req, res) => {
